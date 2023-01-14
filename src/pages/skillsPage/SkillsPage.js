c85e313c18d8493eb2c7dd0a3fb24c89c661d4aa
Lukasnvc <lukasnvc@gmail.com>
2023-01-14 12:21:53 +0200

    skillsPage responsive done

--- src/pages/skillsPage/SkillsPage.js
+++ src/pages/skillsPage/SkillsPage.js
@@ -52,7 +52,30 @@ const SkillsPage = () => {
         </div>
 
         <div className="right">
-          <h1>visa info</h1>
+          <h1 className="title-detail">Programming journey</h1>
+          <ul>
+            <li>
+              <span>2022 summer</span>
+              <p>
+                Started learning Programming basics CSS, HTML, JavaScript
+                reading w3schools, developer.mozilla.org documentations and
+                doing toturials, thats where I got hooked up
+              </p>
+            </li>
+            <li>
+              <span>2022 autumn</span>
+              <p>
+                Started CodeAcademy 7 month Front-End 1-2 level studies. Program
+                included: HTML, CSS, SCSS, JavaScript, intro to Git, Bootstrap,
+                NodeJs, intro to MongoDb and cherry on pie - React. Here I got
+                lots of usefull information using different programming
+                languages solving complex tasks. Studies involved lots of
+                practical assignments as well as theory to clear everythig. So
+                here I got even more hooked up to progamming and spend even more
+                time doing toturials extra from studies.
+              </p>
+            </li>
+          </ul>
         </div>
 
         <Footer />
