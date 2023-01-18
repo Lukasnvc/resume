8a2f86ccf5137bf80e6c1dac93b6b6fd8f6e68f3
Lukasnvc <lukasnvc@gmail.com>
2023-01-18 14:07:12 +0200

    .

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
     <>
       <NavBar />
       <Routes>
-        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/resume" element={<HomePage />}></Route>
         <Route path="/education" element={<EducationPage />}></Route>
         <Route path="/skills" element={<SkillsPage />}></Route>
 
