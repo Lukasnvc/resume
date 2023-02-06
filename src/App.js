70159081492b1b282ba0f592d50be5392fa0ad0d
Lukasnvc <lukasnvc@gmail.com>
2023-02-06 18:07:21 +0200

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 import HomePage from "./pages/homePage/HomePage";
 import ContactsPage from "./pages/contactsPage/ContactsPage";
 import EducationPage from "./pages/educationPage/EducationPage";
