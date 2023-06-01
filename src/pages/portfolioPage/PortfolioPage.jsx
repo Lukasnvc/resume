96cca04cdee9b298ac6a62e44b20a0943c347fa9
Lukasnvc <lukasnvc@gmail.com>
2023-06-01 14:55:42 +0300

    project added

--- src/pages/portfolioPage/PortfolioPage.jsx
+++ src/pages/portfolioPage/PortfolioPage.jsx
@@ -4,13 +4,14 @@ import PorfolioWork from "../../components/portfolioCard/PorfolioWork";
 import { TabTitle } from "../../utils/generalFunctions";
 import cocktailWheel from '../../pictures/cocktailWheel.png';
 import eShop from '../../pictures/eShop.png';
+import moviesApp from '../../pictures/movies-app.png';
 import portfolio from '../../pictures/portfolio.png';
 import quiz from '../../pictures/quiz.png'
 import smartCart from '../../pictures/smartCart.png';
 import styled from "styled-components";
-import techJobs from '../../pictures/techJobs.png'
-import travel from '../../pictures/travel.png'
-import trip from '../../pictures/trip.png'
+import techJobs from '../../pictures/techJobs.png';
+import travel from '../../pictures/travel.png';
+import trip from '../../pictures/trip.png';
 
 const PortfolioPage = () => {
   TabTitle("Portfolio");
@@ -21,6 +22,8 @@ const PortfolioPage = () => {
         <PorfolioWork name={"SmartCart"} link={"https://lukasnvc.github.io/smartcart/"} code={"https://github.com/Lukasnvc/smartcart.git"} pic={smartCart} lang={'React, Styled-components, api'} />
 
         <PorfolioWork name={"Quiz game"} link={"https://lukasnvc.github.io/quiz-app/"} code={"https://github.com/Lukasnvc/quiz-app.git"} pic={quiz} lang={'React, Styled-components, TypeScript, api'} />
+
+        <PorfolioWork name={"MoviesApp"} link={"https://lukasnvc.github.io/movies-app/"} code={"https://github.com/Lukasnvc/movies-app.git"} pic={moviesApp} lang={'React, Scss, Redux Toolkit, api'} />
 
         <PorfolioWork name={"TripApp"} link={"https://expo.dev/@lukasnvc/trip-app?serviceType=classic&distribution=expo-go"} code={"https://github.com/Lukasnvc/trip-app-native.git"} pic={trip} lang={'React Native, Tailwind, api'} />
 
